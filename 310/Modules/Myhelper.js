2901e603410fd11321158d8a6963692190a8ef14
子鵬 李 <hawk419383@gmail.com>
2020-04-09 21:13:51 +0800

    update

---
+++ 310/Modules/Myhelper.js
@@ -0,0 +1,15 @@
+function $g(selector) {
+    if (selector.includes('#') && !selector.includes('')) {
+        return document.querySelector(selector);
+    }
+    let nodelist = document.querySelectorAll(selector);
+    return nodelist.length == 1 ? nodelist[0] : nodelist;
+
+}
+
+function $c(selector) {
+    return document.createElement(selector);
+}
+function $n(selector) {
+    return document.getElementsByClassName(selector);
+}
